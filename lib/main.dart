4ba9bd0d05182ad0545c5f13bd815de559ddc336
Anees Rahman <aneesrahman4440@gmail.com>
2025-06-11 19:25:34 +0530

    Add go route

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
 import 'routes/app_route_configuration.dart';
+import 'providers/counter_provider.dart';
+import 'providers/color_provider.dart';
+import 'providers/calculator_provider.dart';
 
 void main() {
-  runApp(const MiniApps());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => CounterProvider()),
+        ChangeNotifierProvider(create: (_) => ColorProvider()),
+        ChangeNotifierProvider(create: (_) => CalculatorProvider()),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
-class MiniApps extends StatelessWidget {
-  const MiniApps({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      title: 'Mini Apps',
-      theme: ThemeData(primarySwatch: Colors.blue),
+      title: 'Mini Apps with GoRouter + Provider',
+      theme: ThemeData(primarySwatch: Colors.teal),
       routerConfig: MyRouterApp.router,
     );
   }
